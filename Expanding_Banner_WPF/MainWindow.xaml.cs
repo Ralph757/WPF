cb692dd0a7a8c74c9ef776e8ab689b5020c5491e
Raphael_Hilario <60021717+Ralph757@users.noreply.github.com>
2020-03-15 00:10:58 -0700

    Add files via upload

---
+++ Expanding_Banner_WPF/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Banner_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //private void InitializeComponent()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        private void Banner(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation doubleanimation = new DoubleAnimation();
+            doubleanimation.From = 2;
+            doubleanimation.To = 990;
+            doubleanimation.Duration = TimeSpan.FromSeconds(1); 
+
+            
+            banner.BeginAnimation(WidthProperty, doubleanimation);
+
+        }
+
+       
+    }
+}
